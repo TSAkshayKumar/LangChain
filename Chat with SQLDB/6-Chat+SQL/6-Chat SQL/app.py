87d5bc038c3f6237ceaf5cef0cb82436c03ff952
Tsakshaykumar <akshaykumar25100@gmail.com>
2025-03-22 18:17:30 +0530

    chat with db

---
+++ Chat with SQLDB/6-Chat+SQL/6-Chat SQL/app.py
@@ -0,0 +1,105 @@
+import streamlit as st
+from pathlib import Path # module provides an object-oriented way to handle filesystem paths in Python.
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3
+from langchain_groq import ChatGroq
+
+st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 LangChain: Chat with SQL DB")
+# var
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
+
+radio_opt=["Use SQLLite 3 Database- Student.db","Connect to you MySQL Database"]
+
+selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1: # if MY SQL DB selected then get details
+    db_uri=MYSQL
+    mysql_host=st.sidebar.text_input("Provide MySQL Host")
+    mysql_user=st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db=st.sidebar.text_input("MySQL database")
+else:
+    db_uri=LOCALDB # no extra details needed
+
+api_key=st.sidebar.text_input(label="GRoq API Key",type="password")
+
+if not db_uri:
+    st.info("Please enter the database information and uri")
+
+if not api_key:
+    st.info("Please add the groq api key")
+
+## LLM model
+llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"student.db").absolute() 
+        # Path(__file__) current python script python .parent "6-Chat SQL" return absolute path
+        
+        # The file: prefix is used in SQLite connection strings to explicitly indicate that the connection is a URI (Uniform Resource Identifier) rather than a simple file path("file_name.db").
+        
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True) # defines lambda function creator.
+
+        # However, using file: alone is not enough for SQLite to treat it as a URI. You must also include uri=True in the sqlite3.connect() function to ensure SQLite interprets the path correctly.
+        
+        
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+        # This line is creating and returning a SQLDatabase object
+        # The number of slashes (///) indicates:
+            # 3 slashes (///) → SQLite database with a relative path (database file located in your project folder).
+            # 4 slashes (////) → SQLite database with an absolute path (full path from the root directory).                           
+    
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))   
+    
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db=configure_db(db_uri)
+    #db contains SQLDatabase object
+
+## toolkit
+toolkit=SQLDatabaseToolkit(db=db,llm=llm)
+
+agent=create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+)
+
+# clear history
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+# write on screen
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query=st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback=StreamlitCallbackHandler(st.container())
+        response=agent.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
+
+        
+
+
